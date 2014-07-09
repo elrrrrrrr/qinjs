3b269232007b513c42201cf7933f44c1fafa02cb
elrrrrrrr <elrrrrrrr@gmail.com>
2014-07-09 23:09:45 +0800

    remove jest

--- __tests__/simple.js
+++
@@ -1,9 +0,0 @@
-jest.dontMock('../qin.js');
-describe('test', function() {
- it('adds 1 + 2 to equal 3', function() {
-   var sum = function(a,b){
-   	return a+b;
-   }
-   expect(sum(1, 2)).toBe(3);
- });
-});
