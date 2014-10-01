dec99e7a5edaafab87a7b5b293e085a6803605ef
elrrrrrrr <elrrrrrrr@gmail.com>
2014-10-01 23:16:21 +0800

    update the comments

--- qin.js
+++ qin.js
@@ -1,28 +1,31 @@
 
-/*globals Node:true, NodeList:true*/
+/**
+ * 需要浏览器支持HTML node对象及querySelect方法
+ * @param  {[document]} document [document对象]
+ * @param  {[window]} window   [window对象]
+ * @param  {[qinjs]} $        [浏览器实例,干掉comand tool]
+ */
 $ = (function (document, window, $) {
   
   var node = Node.prototype,
       trigger = 'trigger',
-      // note: createElement requires a string in Firefox
+      // 兼容firefox, firefox下创建元素需要一个字符串作为参数
       dummy = document.createElement('i');
-
+  //为nodeList添加遍历方法
   NodeList.prototype.forEach = [].forEach
-  // we have to explicitly add a window.on as it's not included
-  // in the Node object.
+  //为node对象添加on方法 
   window.on = Node.prototype.on = function (event, fn) {
     this.addEventListener(event, fn, false);
-
     return this;
   };
-
+  //nodeList遍历添加
   NodeList.prototype.on = function (event, fn) {
     this[forEach](function (el) {
       el.on(event, fn);
     });
     return this;
   };
-
+  //封装node style属性
   Node.prototype.css = function(k,v){
     var self = this;
     typeof k != 'object' ? this.style[k] = v : (function(k){
@@ -32,7 +35,7 @@ $ = (function (document, window, $) {
     })(k)
     return this ;
   }
-
+  //同理递归实现
   NodeList.prototype.css = function(k,v){
     this.forEach(
       function(el){
@@ -41,42 +44,38 @@ $ = (function (document, window, $) {
     )
     return this;
   }
-  // we save a few bytes (but none really in compression)
-  // by using [trigger] - really it's for consistency in the
-  // source code.
+  //添加自定义事件支持
   window[trigger] = Node.prototype[trigger] = function (type, data) {
-    // construct an HTML event. This could have
-    // been a real custom event
+    //模拟事件
     var event = document.createEvent('HTMLEvents');
     event.initEvent(type, true, true);
     event.data = data || {};
     event.eventName = type;
     event.target = this;
+    //触发事件
     this.dispatchEvent(event);
+    //链式调用
     return this;
   };
-
+  //遍历调用
   NodeList.prototype[trigger] = function (event) {
     this[forEach](function (el) {
       el[trigger](event);
     });
     return this;
   };
-
+  
+  //创建实例 s 为选择器元素
   $ = function (s) {
-    
+    //获取node对象
     var r = document.querySelectorAll(s || '☺'),
         length = r.length;
-    
+    //单个元素直接返回,多个元素返回nodeList
     return length == 1 ? r[0] : r;
   };
-
-  $.test = function(){
-    alert('test')
-  }
-  
+  //获取node.on(),重新指定作用域,完成自定义事件支持  
   $.on = Node.prototype.on.bind(dummy);
   $[trigger] = Node.prototype[trigger].bind(dummy);
 
   return $;
-})(document, this);
+})(document, window);
